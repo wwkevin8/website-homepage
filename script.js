866c7cd06af37a117c158e054ff37f8f773ebe5c
Kevin Wang <1610777313@qq.com>
2026-03-08 17:13:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿const footerYear = document.querySelector("#footer-year");
+
+if (footerYear) {
+  footerYear.textContent = new Date().getFullYear() + " Nottingham Good Neighbor. All rights reserved.";
+}
